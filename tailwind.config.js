17fcc7898ab3a6c2616be25b94578d540e7e28eb
Dominic Huster <d.huster@lido-fenster.de>
2024-09-26 18:27:58 +0200

    .

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        accent: "#8B0000", // tiefes Rot für Akzente
+      },
+    },
+  },
+  plugins: [],
+};
