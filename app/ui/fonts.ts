5a61e0ce29cbb1437a1ba4d0a4f60ae2f31729ed
Dominic Huster <d.huster@lido-fenster.de>
2024-12-28 14:09:42 +0900

    New Start

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,9 @@
 import { Inter, Lusitana } from "next/font/google";
+// import { LisztFY } from "app/dashboard/fonts";
 
 export const inter = Inter({ subsets: ["latin"] });
 export const lusitana = Lusitana({
   weight: ["400", "700"],
   subsets: ["latin"],
 });
+// export const lisztfy = LisztFY();
